4b9e60f34bfd545213c84b7261101c4fb64402ba
Carlos <ctey@divisait.com>
2023-12-25 12:02:38 +0100

    Logica del boton select en la Home

--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { Select } from "../components/Select";
 import { NavLink } from "react-router-dom";
 //@ts-ignore
 import triviaLogo from "../assets/images/triviaLogo.png";
@@ -8,7 +9,8 @@ import { useState,useEffect } from "react";
 
 const HomeContainer = () =>{
     const [nameUser, setNameUser]= useState("");
-    const [stateButton, setStateButton]= useState(true); 
+    const [stateButton, setStateButton]= useState(true);
+    const [selectResults, setSelector] = useState('all'); 
 
     useEffect(() => {
        if(nameUser != ""){ //Compruebo si el campo "Username" es distinto de vacio
@@ -19,6 +21,11 @@ const HomeContainer = () =>{
         localStorage.setItem('userName', '');
        }
     }, [nameUser]);
+
+
+    useEffect(() => {
+        localStorage.setItem('difficulty', selectResults);
+    }, [selectResults]);
     
     return (
         <div className="homeContainer">
@@ -33,8 +40,16 @@ const HomeContainer = () =>{
                         handleInputChange={(e: React.ChangeEvent<HTMLInputElement>) => setNameUser(e.target.value)}
                         placeholder="Username"
                         />
+                        <p className="labelDifuculty">Enter difficulty</p>
+                        <Select 
+                            value={selectResults} 
+                            clasname="selector"
+                            handleInputChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                            setSelector(e.target.value)}
+                        />
                     </div>
                     <div className="buttonnHomeContainer">
+                    
                     <NavLink to="/questions" >
                         <Button clasname="startButton" text="Start" disabled={stateButton}/>
                     </NavLink>
